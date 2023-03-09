e4640d12477f5a21032f37cd95079c852380bada
johnny cunningham <jcunningham@aemcorp.com>
2023-03-09 16:40:16 -0500

    Copy out FactK12StudentDisciplines

--- generate.database/VersionUpdates/6.0_prerelease/V10Upgrade/Copy-Out-Legacy-Data.sql
+++ generate.database/VersionUpdates/6.0_prerelease/V10Upgrade/Copy-Out-Legacy-Data.sql
@@ -194,3 +194,188 @@ JOIN RDS.DimK12EnrollmentStatuses rdkes ON f.K12EnrollmentStatusId = rdkes.DimK1
 JOIN RDS.DimSeas rds ON f.SeaId = rds.DimSeaId
 JOIN RDS.DimIeus rdi ON f.IeuId = rdi.DimIeuId
 JOIN RDS.DimDates rdsesed ON f.SpecialEducationServicesExitDateId = rdsesed.DimDateId
+
+
+
+/*******************************************************************************************************/
+
+
+--Why is Race not in the new table
+--We have other statuses but not Military Connected or Section504, is that correct
+
+
+/*
+CREATE TABLE [RDS].[tmp_ms_xx_FactK12StudentDisciplines] (
+    [FactK12StudentDisciplineId]        INT             IDENTITY (1, 1) NOT NULL,
+    [SchoolYearId]                      INT             CONSTRAINT [DF_FactK12StudentDisciplines_SchoolYearId] DEFAULT ((-1)) NOT NULL,
+    [FactTypeId]                        INT             CONSTRAINT [DF_FactK12StudentDisciplines_FactTypeId] DEFAULT ((-1)) NOT NULL,
+    [SeaId]                             INT             CONSTRAINT [DF_FactK12StudentDisciplines_SeaId] DEFAULT ((-1)) NOT NULL,
+    [IeuId]                             INT             CONSTRAINT [DF_FactK12StudentDisciplines_IeuId] DEFAULT ((-1)) NOT NULL,
+    [LeaId]                             INT             CONSTRAINT [DF_FactK12StudentDisciplines_LeaId] DEFAULT ((-1)) NOT NULL,
+    [K12SchoolId]                       INT             CONSTRAINT [DF_FactK12StudentDisciplines_K12SchoolId] DEFAULT ((-1)) NOT NULL,
+    [K12StudentId]                      INT             CONSTRAINT [DF_FactK12StudentDisciplines_K12StudentId] DEFAULT ((-1)) NOT NULL,
+    [AgeId]                             INT             CONSTRAINT [DF_FactK12StudentDisciplines_AgeId] DEFAULT ((-1)) NOT NULL,
+    [CteStatusId]                       INT             CONSTRAINT [DF_FactK12StudentDisciplines_CteStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [DisabilityStatusId]                INT             CONSTRAINT [DF_FactK12StudentDisciplines_DisabilityStatusId] DEFAULT ((-1)) NOT NULL,
+    [DisciplinaryActionStartDateId]     INT             CONSTRAINT [DF_FactK12StudentDisciplines_DisciplinaryActionStartDateId] DEFAULT ((-1)) NOT NULL,
+NEW    [DisciplinaryActionEndDateId]       INT             CONSTRAINT [DF_FactK12StudentDisciplines_DisciplinaryActionEndDateId] DEFAULT ((-1)) NOT NULL,
+    [DisciplineStatusId]                INT             CONSTRAINT [DF_FactK12StudentDisciplines_DisciplineStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [EconomicallyDisadvantagedStatusId] INT             CONSTRAINT [DF_FactK12StudentDisciplines_EconomicallyDisadvantagedStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [EnglishLearnerStatusId]            INT             CONSTRAINT [DF_FactK12StudentDisciplines_EnglishLearnerStatusId] DEFAULT ((-1)) NOT NULL,
+    [FirearmId]                         INT             CONSTRAINT [DF_FactK12StudentDisciplines_FirearmId] DEFAULT ((-1)) NOT NULL,
+    [FirearmDisciplineStatusId]         INT             CONSTRAINT [DF_FactK12StudentDisciplines_FirearmDisciplineStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [FosterCareStatusId]                INT             CONSTRAINT [DF_FactK12StudentDisciplines_FosterCareStatusId] DEFAULT ((-1)) NOT NULL,
+    [GradeLevelId]                      INT             CONSTRAINT [DF_FactK12StudentDisciplines_GradeLevelId] DEFAULT ((-1)) NOT NULL,
+NEW    [HomelessnessStatusId]              INT             CONSTRAINT [DF_FactK12StudentDisciplines_HomelessnessStatusId] DEFAULT ((-1)) NOT NULL,
+    [IdeaStatusId]                      INT             CONSTRAINT [DF_FactK12StudentDisciplines_IdeaStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [ImmigrantStatusId]                 INT             CONSTRAINT [DF_FactK12StudentDisciplines_ImmigrantStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [IncidentIdentifier]                NVARCHAR (40)   NULL,
+NEW    [IncidentStatusId]                  INT             CONSTRAINT [DF_FactK12StudentDisciplines_IncidentStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [IncidentDateId]                    INT             CONSTRAINT [DF_FactK12StudentDisciplines_IncidentDateId] DEFAULT ((-1)) NOT NULL,
+    [K12DemographicId]                  INT             CONSTRAINT [DF_FactK12StudentDisciplines_K12DemographicId] DEFAULT ((-1)) NOT NULL,
+NEW    [MigrantStatusId]                   INT             CONSTRAINT [DF_FactK12StudentDisciplines_MigrantId] DEFAULT ((-1)) NOT NULL,
+NEW    [NOrDStatusId]                      INT             CONSTRAINT [DF_FactK12StudentDisciplines_NOrDStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [PrimaryDisabilityTypeId]           INT             CONSTRAINT [DF_FactK12StudentDisciplines_PrimaryDisabilityTypeId] DEFAULT ((-1)) NOT NULL,
+NEW    [SecondaryDisabilityTypeId]         INT             CONSTRAINT [DF_FactK12StudentDisciplines_SecondaryDisabilityTypeId] DEFAULT ((-1)) NOT NULL,
+NEW    [TitleIStatusId]                    INT             CONSTRAINT [DF_FactK12StudentDisciplines_TitleIStatusId] DEFAULT ((-1)) NOT NULL,
+NEW    [TitleIIIStatusId]                  INT             CONSTRAINT [DF_FactK12StudentDisciplines_TitleIIIStatusId] DEFAULT ((-1)) NOT NULL,
+    [DurationOfDisciplinaryAction]      DECIMAL (18, 2) NULL,
+    [DisciplineCount]                   INT             CONSTRAINT [DF_FactK12StudentDisciplines_Id] DEFAULT ((1)) NULL,
+    CONSTRAINT [tmp_ms_xx_constraint_PK_FactK12StudentDisciplines1] PRIMARY KEY CLUSTERED ([FactK12StudentDisciplineId] ASC) WITH (FILLFACTOR = 80)
+);
+*/
+
+CREATE TABLE Upgrade.FactK12StudentDisciplines (
+	DisciplineCount INT
+	, SchoolYear SMALLINT
+	, GradeLevelCode NVARCHAR(200)
+	, AgeCode NVARCHAR(200)
+
+	, DisciplinaryActionTakenCode NVARCHAR(200)
+	, DisciplineMethodOfChildrenWithDisabilitiesCode NVARCHAR(200)
+	, EducationalServicesAfterRemovalCode NVARCHAR(200)
+	, IdeaInterimRemovalReasonCode NVARCHAR(200)
+	, IdeaInterimRemovalCode NVARCHAR(200)
+	, DisciplineELStatusCode NVARCHAR(200)
+    , FirearmTypeCode NVARCHAR(200)
+    , DisciplineMethodForFirearmsIncidentsCode NVARCHAR(200)
+	, IdeaDisciplineMethodForFirearmsIncidentsCode NVARCHAR(200)
+    , DisciplinaryActionStartDate DATETIME
+    , DurationOfDisciplinaryAction DECIMAL(18,2)
+
+	, SeaIdentifierState NVARCHAR(200)
+	, SEA_RecordStartDateTime DATETIME
+	, SEA_RecordEndDateTime	  DATETIME
+	, IeuIdentifierState NVARCHAR(200)
+	, IEU_RecordStartDateTime DATETIME
+	, IEU_RecordEndDateTime	  DATETIME
+	, SchoolIdentifierState NVARCHAR(200)
+	, SCH_RecordStartDateTime DATETIME
+	, SCH_RecordEndDateTime	  DATETIME
+	, StateStudentIdentifier NVARCHAR(200)
+	, STU_RecordStartDateTime DATETIME
+	, STU_RecordEndDateTime	  DATETIME
+	, LeaIdentifierState NVARCHAR(200)
+	, LEA_RecordStartDateTime DATETIME
+	, LEA_RecordEndDateTime	  DATETIME
+
+	, EnglishLearnerStatusCode NVARCHAR(200)
+	, IdeaIndicatorCode NVARCHAR(200)
+	, PrimaryDisabilityTypeCode NVARCHAR(200)
+
+	, rdps.EligibilityStatusForSchoolFoodServiceProgramCode
+	, rdps.FosterCareProgramCode
+	, rdps.HomelessServicedIndicatorCode
+	, rdps.Section504StatusCode
+	, rdps.TitleIIIImmigrantParticipationStatusCode
+	, rdps.TitleiiiProgramParticipationCode
+
+	, LepPerkinsStatusCode NVARCHAR(200)
+	, CteAeDisplacedHomemakerIndicatorCode NVARCHAR(200)
+	, CteGraduationRateInclusionCode NVARCHAR(200)
+	, CteNontraditionalGenderStatusCode NVARCHAR(200)
+	, CteProgramCode NVARCHAR(200)
+	, LepPerkinsStatusCode NVARCHAR(200)
+	, RepresentationStatusCode NVARCHAR(200)
+	, SingleParentOrSinglePregnantWomanCode NVARCHAR(200)
+	)
+
+
+
+
+
+INSERT INTO Upgrade.FactK12StudentDisciplines
+SELECT 
+	f.DisciplineCount
+	, rdsy.SchoolYear
+	, rdgl.GradeLevelCode
+	, rda.AgeCode
+
+	, rdd.DisciplinaryActionTakenCode
+	, rdd.DisciplineMethodOfChildrenWithDisabilitiesCode
+	, rdd.EducationalServicesAfterRemovalCode
+	, rdd.IdeaInterimRemovalReasonCode
+	, rdd.IdeaInterimRemovalCode
+	, rdd.DisciplineELStatusCode
+    , rdf.FirearmTypeCode
+    , rdfd.DisciplineMethodForFirearmsIncidentsCode
+	, rdfd.IdeaDisciplineMethodForFirearmsIncidentsCode
+    , f.DisciplinaryActionStartDate
+    , f.DisciplineDuration
+
+	, rds.SeaIdentifierState
+	, rds.RecordStartDateTime AS SEA_RecordStartDateTime
+	, rds.RecordEndDateTime   AS SEA_RecordEndDateTime
+	, rdi.IeuIdentifierState
+	, rdi.RecordStartDateTime AS IEU_RecordStartDateTime
+	, rdi.RecordEndDateTime	  AS IEU_RecordEndDateTime
+	, rdlea.LeaIdentifierState
+	, rdlea.RecordStartDateTime AS LEA_RecordStartDateTime
+	, rdlea.RecordEndDateTime	AS LEA_RecordEndDateTime
+	, rdksch.SchoolIdentifierState
+	, rdksch.RecordStartDateTime AS SCH_RecordStartDateTime
+	, rdksch.RecordEndDateTime	 AS SCH_RecordEndDateTime
+	, rdkstu.StateStudentIdentifier
+	, rdkstu.RecordStartDateTime  AS STU_RecordStartDateTime
+	, rdkstu.RecordEndDateTime	  AS STU_RecordEndDateTime
+
+	, rdkd.EconomicDisadvantageStatusCode
+	, rdkd.EnglishLearnerStatusCode
+	, rdis.IdeaIndicatorCode
+	, rdis.PrimaryDisabilityTypeCode
+
+	, rdps.EligibilityStatusForSchoolFoodServiceProgramCode
+	, rdps.FosterCareProgramCode
+	, rdps.HomelessServicedIndicatorCode
+	, rdps.Section504StatusCode
+	, rdps.TitleIIIImmigrantParticipationStatusCode
+	, rdps.TitleiiiProgramParticipationCode
+
+	, rdctes.CteAeDisplacedHomemakerIndicatorCode
+	, rdctes.CteGraduationRateInclusionCode
+	, rdctes.CteNontraditionalGenderStatusCode
+	, rdctes.CteProgramCode
+	, rdctes.LepPerkinsStatusCode
+	, rdctes.RepresentationStatusCode
+	, rdctes.SingleParentOrSinglePregnantWomanCode
+
+FROM RDS.FactK12StudentDisciplines f
+JOIN RDS.DimSchoolYears rdsy ON f.SchoolYearId = rdsy.DimSchoolYearId
+JOIN RDS.DimGradeLevels rdgl ON f.GradeLevelId = rdgl.DimGradeLevelId
+JOIN RDS.DimAges rda ON f.AgeId = rda.DimAgeId
+JOIN RDS.DimRaces rdr ON f.RaceId = rdr.DimRaceId
+--JOIN RDS.DimFactTypes rdft ON f.FactTypeId = rdft.DimFactTypeId
+JOIN RDS.DimDisciplines rdd ON f.DisciplineId = rdd.DimDisciplineId
+JOIN RDS.DimFirearms rdf ON f.FirearmsId = rdf.DimFirearmsId
+JOIN RDS.DimFirearmDisciplines rdfd ON f.FirearmDisciplineId = rdfd.DimFirearmDisciplineId
+
+JOIN RDS.DimSeas rds ON f.SeaId = rds.DimSeaId
+LEFT JOIN RDS.DimIeus rdi ON f.IeuId = rdi.DimIeuId
+JOIN RDS.DimLeas rdlea ON f.LeaId = rdlea.DimLeaID
+JOIN RDS.DimK12Schools rdksch ON f.K12SchoolId = rdksch.DimK12SchoolId
+JOIN RDS.DimK12Students rdkstu ON f.K12StudentId = rdkstu.DimK12StudentId
+
+JOIN RDS.DimK12Demographics rdkd ON f.K12DemographicId = rdkd.DimK12DemographicId
+JOIN RDS.DimIdeaStatuses rdis ON f.IdeaStatusId = rdis.DimIdeaStatusId
+JOIN RDS.DimCteStatuses rdctes ON f.CteStatusId = rdctes.DimCteStatusId
+JOIN RDS.DimProgramStatuses rdps ON f.ProgramStatusId = rdps.DimProgramStatusId
