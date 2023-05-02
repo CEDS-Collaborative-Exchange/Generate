be386ebe6bafa4f5e6ef0931bde47da744eb6a93
jwheaton2963 <jwheaton@msfw.com>
2023-05-02 15:55:07 -0500

    Initial checkin of Wrapper

--- generate.database/StoredProcedures/Create/App.Wrapper_Migrate_Membership_to_RDS.StoredProcedure.sql
+++ generate.database/StoredProcedures/Create/App.Wrapper_Migrate_Membership_to_RDS.StoredProcedure.sql
@@ -6,48 +6,74 @@ BEGIN
 
 	BEGIN TRY
 
+		EXEC Staging.Rollover_SourceSystemReferenceData -- This only happens when it is needed
+
 	--Populate the RDS tables from ODS data
 			--write out message to DataMigrationHistories
 			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start MigrateDimStudents')
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Staging-to-DimK12Students')
 
 		--Populate DimStudents
-		exec [rds].[Migrate_DimK12Students]
+		--exec [rds].[Migrate_DimK12Students] NULL
+		exec [Staging].[Staging-to-DimK12Students] NULL
 
 			--write out message to DataMigrationHistories
 			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Migrate_DimSeas')
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Staging-to-DimSeas')
 
 		--Populate DimSeas
-		exec [rds].[Migrate_DimSeas] 'directory', NULL, 0
+		exec [Staging].[Staging-to-DimSeas] 'directory', NULL, 0
 
 			--write out message to DataMigrationHistories
 			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Migrate_DimLeas')
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Staging-to-DimLeas')
 
 		--Populate DimLeas
-		exec [rds].[Migrate_DimLeas] 'directory', NULL, 0
+		exec [Staging].[Staging-to-DimLeas] 'directory', NULL, 0
 
 			--write out message to DataMigrationHistories
 			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Migrate_DimK12Schools')
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Staging-to-DimK12Schools')
 
 		--Populate DimK12Schools
-		exec [rds].[Migrate_DimK12Schools] NULL, 0
+		exec [Staging].[Staging-to-DimK12Schools] NULL, 0
 
 			--write out message to DataMigrationHistories
 			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Empty for Membership')
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Empty_RDS for the Submission reports')
 
-		--clear the data from the fact table
+
+			--write out message to DataMigrationHistories
+			insert into app.DataMigrationHistories
+			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Empty_RDS for the Submission reports')
+
+		----clear the data from the fact table
 		exec [rds].[Empty_RDS] 'membership'
 
-			--write out message to DataMigrationHistories
-			insert into app.DataMigrationHistories
-			(DataMigrationHistoryDate, DataMigrationTypeId, DataMigrationHistoryMessage) values	(getutcdate(), 2, 'RDS Migration Wrapper Membership - Start Migrate_StudentCounts for Submission reports')
+		
+		DECLARE @submissionYear AS VARCHAR(50)
+		DECLARE selectedYears_cursor CURSOR FOR 
+		SELECT d.SchoolYear
+			FROM rds.DimSchoolYears d
+			JOIN rds.DimSchoolYearDataMigrationTypes dd ON dd.DimSchoolYearId = d.DimSchoolYearId
+			JOIN App.DataMigrationTypes b ON b.DataMigrationTypeId=dd.DataMigrationTypeId 
+			WHERE d.DimSchoolYearId <> -1 
+			AND dd.IsSelected=1 AND DataMigrationTypeCode='RDS'
 
-		--populate the fact table for the submission report
-		exec [rds].[Migrate_StudentCounts] 'membership', 0
+
+		OPEN selectedYears_cursor
+		FETCH NEXT FROM selectedYears_cursor INTO @submissionYear
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			EXEC Staging.[Staging-to-FactK12StudentCounts_Membership] @submissionYear
+
+			FETCH NEXT FROM selectedYears_cursor INTO @submissionYear
+		END
+		
+		CLOSE selectedYears_cursor
+		DEALLOCATE selectedYears_cursor
+
+
 
 	--RDS migration complete
 			--write out message to DataMigrationHistories
@@ -63,6 +89,3 @@ BEGIN
 	SET NOCOUNT OFF;
 
 END
-
-
-
