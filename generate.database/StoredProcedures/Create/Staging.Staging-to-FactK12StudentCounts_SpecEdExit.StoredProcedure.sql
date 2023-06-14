49a2b947163bceb9349466eb681bacea1f3b544a
johnny cunningham <jcunningham@aemcorp.com>
2023-06-13 21:31:48 -0400

    v11 fixes

--- generate.database/StoredProcedures/Create/Staging.Staging-to-FactK12StudentCounts_SpecEdExit.StoredProcedure.sql
+++ generate.database/StoredProcedures/Create/Staging.Staging-to-FactK12StudentCounts_SpecEdExit.StoredProcedure.sql
@@ -274,7 +274,7 @@ BEGIN
 			AND IdeaIndicatorCode = 'Yes'
 			AND rdis.IdeaEducationalEnvironmentForSchoolAgeCode = 'MISSING'
 		LEFT JOIN RDS.vwDimIdeaDisabilityTypes rdidt
-			ON ske.SchoolYear = rdis.SchoolYear
+			ON sidt.SchoolYear = rdidt.SchoolYear
 			AND ISNULL(sidt.IdeaDisabilityTypeCode, 'MISSING') = ISNULL(rdidt.IdeaDisabilityTypeMap, rdidt.IdeaDisabilityTypeCode)
 			AND sidt.IsPrimaryDisability = 1
 		LEFT JOIN #vwRaces rdr
