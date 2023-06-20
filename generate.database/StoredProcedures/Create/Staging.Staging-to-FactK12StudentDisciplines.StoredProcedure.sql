7c3793a5effed55d9e51143e8b6582a30cc67def
johnny cunningham <jcunningham@aemcorp.com>
2023-06-20 14:39:38 -0400

    v11 discipline updates

--- generate.database/StoredProcedures/Create/Staging.Staging-to-FactK12StudentDisciplines.StoredProcedure.sql
+++ generate.database/StoredProcedures/Create/Staging.Staging-to-FactK12StudentDisciplines.StoredProcedure.sql
@@ -2,30 +2,30 @@
 Author: AEM Corp
 Date:	5/1/2022
 Description: Migrates Discipline Data from Staging to RDS.FactK12StudentDisciplines
-
+ 
 NOTE: This Stored Procedure processes files: 005, 006, 007, 086, 088, 143, 144
 ************************************************************************/
-
+ 
 CREATE PROCEDURE [Staging].[Staging-to-FactK12StudentDisciplines]
 	@SchoolYear SMALLINT
 AS
 BEGIN
 	 --SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
 	SET NOCOUNT ON;
-
+ 
 -- Drop temp tables.  This allows for running the procedure as a script while debugging
 		IF OBJECT_ID(N'tempdb..#vwGradeLevels') IS NOT NULL DROP TABLE #vwGradeLevels
 		IF OBJECT_ID(N'tempdb..#vwRaces') IS NOT NULL DROP TABLE #vwRaces
 		IF OBJECT_ID(N'tempdb..#vwIdeaStatuses') IS NOT NULL DROP TABLE #vwIdeaStatuses
 		IF OBJECT_ID(N'tempdb..#vwUnduplicatedRaceMap') IS NOT NULL DROP TABLE #vwUnduplicatedRaceMap
 		IF OBJECT_ID(N'tempdb..#vwEnglishLearnerStatuses') IS NOT NULL DROP TABLE #vwEnglishLearnerStatuses		
-		IF OBJECT_ID('tempdb.dbo.#vwDisciplinesStatuses', 'U') IS NOT NULL DROP TABLE #vwDisciplinesStatuses
+		IF OBJECT_ID('tempdb.dbo.#vwDisciplineStatuses', 'U') IS NOT NULL DROP TABLE #vwDisciplineStatuses
 		IF OBJECT_ID('tempdb..#Facts') IS NOT NULL DROP TABLE #Facts
 		IF OBJECT_ID('tempdb.dbo.#tempELStatus', 'U') IS NOT NULL DROP TABLE #tempELStatus
 	
-
+ 
 	BEGIN TRY
-
+ 
 		DECLARE 
 		@FactTypeId INT,
 		@SchoolYearId INT,
@@ -37,10 +37,10 @@ BEGIN
 		SELECT @SchoolYearId = DimSchoolYearId 
 		FROM RDS.DimSchoolYears
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		SELECT @StartDate = CAST('7/1/' + CAST(@SchoolYear - 1 AS VARCHAR(4)) AS DATE)
 		SELECT @EndDate = CAST('6/30/' + CAST(@SchoolYear  AS VARCHAR(4)) AS DATE)
-
+ 
 		
 		DECLARE @DimSeaId int
 		SELECT @DimSeaId = (
@@ -48,13 +48,13 @@ BEGIN
 		WHERE RecordStartDateTime between @StartDate and @EndDate
 		ORDER BY RecordStartDateTime)
 		
-
+ 
 	--Set the Child Count date
 		SELECT @ChildCountDate = tr.ResponseValue
 		FROM App.ToggleQuestions tq
 		JOIN App.ToggleResponses tr ON tq.ToggleQuestionId = tr.ToggleQuestionId
 		WHERE tq.EmapsQuestionAbbrv = 'CHDCTDTE'
-
+ 
 		SELECT @ChildCountDate = CAST(CAST(@SchoolYear - 1 AS CHAR(4)) + '-' + CAST(MONTH(@ChildCountDate) AS VARCHAR(2)) + '-' + CAST(DAY(@ChildCountDate) AS VARCHAR(2)) AS DATE)
 	
 	
@@ -64,50 +64,51 @@ BEGIN
 		INTO #vwGradeLevels
 		FROM RDS.vwDimGradeLevels
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		CREATE CLUSTERED INDEX ix_tempvwGradeLevels ON #vwGradeLevels (GradeLevelTypeDescription, GradeLevelMap);
 		
 		SELECT *
 		INTO #vwIdeaStatuses
 		FROM RDS.vwDimIdeaStatuses
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		CREATE CLUSTERED INDEX ix_tempvwIdeaStatuses ON #vwIdeaStatuses (IdeaIndicatorMap, IdeaEducationalEnvironmentForSchoolageMap);
-
+ 
 		SELECT * 
 		INTO #vwRaces 
 		FROM RDS.vwDimRaces
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		CREATE CLUSTERED INDEX ix_tempvwRaces ON #vwRaces (RaceMap);
-
+ 
 		SELECT * 
 		INTO #vwEnglishLearnerStatuses 
 		FROM RDS.vwDimEnglishLearnerStatuses
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		CREATE CLUSTERED INDEX ix_tempvwEnglishLearnerStatuses ON #vwEnglishLearnerStatuses (EnglishLearnerStatusMap)
-
+ 
 		SELECT * 
 		INTO #vwUnduplicatedRaceMap 
 		FROM RDS.vwUnduplicatedRaceMap
 		WHERE SchoolYear = @SchoolYear
-
+ 
 		CREATE CLUSTERED INDEX ix_tempvwUnduplicatedRaceMap ON #vwUnduplicatedRaceMap (StudentIdentifierState, LeaIdentifierSeaAccountability, SchoolIdentifierSea, RaceMap);
-
+ 
 		SELECT * 
-		INTO #vwDisciplinesStatuses 
+		INTO #vwDisciplineStatuses 
 		FROM RDS.vwDimDisciplineStatuses 
 		WHERE SchoolYear = @SchoolYear
 		
-		CREATE INDEX IX_vwDimDisciplines ON #vwDisciplinesStatuses(SchoolYear, IdeaInterimRemovalMap, IdeaInterimRemovalReasonMap) INCLUDE (IdeaInterimRemovalCode, IdeaInterimRemovalReasonCode)
-
-
+		CREATE INDEX IX_vwDimDisciplines ON #vwDisciplineStatuses(SchoolYear, IdeaInterimRemovalMap, IdeaInterimRemovalReasonMap) 
+			INCLUDE (IdeaInterimRemovalCode, IdeaInterimRemovalReasonCode)
+ 
+ 
 	--Set the Fact Type
 		SELECT @FactTypeId = DimFactTypeId 
 		FROM rds.DimFactTypes
 		WHERE FactTypeCode = 'submission'
-
+ 
 	----Pull the EL Status into a temp table
 		SELECT DISTINCT 
 			StudentIdentifierState
@@ -118,17 +119,17 @@ BEGIN
 			, EnglishLearner_StatusEndDate
 		INTO #tempELStatus
 		FROM Staging.PersonStatus
-
+ 
 		CREATE INDEX IX_tempELStatus ON #tempELStatus(StudentIdentifierState, LeaIdentifierSeaAccountability, SchoolIdentifierSea, englishlearner_statusstartdate, englishlearner_statusenddate)-- INCLUDE (IdeaInterimRemovalCode, IdeaInterimRemovalReasonCode, DisciplineELStatusCode)
-
+ 
 	-- Clear the Fact table for the SY being migrated
 		DELETE RDS.FactK12StudentDisciplines
 		WHERE SchoolYearId = @SchoolYearId 
-
+ 
 		IF OBJECT_ID('tempdb..#Facts') IS NOT NULL DROP TABLE #Facts
-
+ 
 	/*  Creating and load #Facts temp table */
-
+ 
 		CREATE TABLE #Facts (
 			StagingId int not null							
 			, AgeId	int null
@@ -137,12 +138,10 @@ BEGIN
 			, DisciplineId int null
 			, FactTypeId	int null
 			, IdeaStatusId	int null
-			--, ProgramStatusId	int null
 			, K12SchoolId	int null
 			, K12StudentId	int null
 			, DisciplineCount	int null
 			, FirearmId 	int null
-			--, DisciplineDuration decimal(18,2) null
 			, GradeLevelId	int null
 			, CTEStatusId	int null
 			, LEAId	int null			
@@ -160,6 +159,7 @@ BEGIN
 			, FirearmDisciplineStatusId int null --new
 			, FosterCareStatusId int null --new
 			, HomelessnessStatusId int null --new
+			, IdeaDisabilityTypeId int null --new
 			, ImmigrantStatusId int null --new
 			, IncidentIdentifier int null --new
 			, IncidentStatusId int null --new
@@ -171,58 +171,58 @@ BEGIN
 			, SecondaryDisabilityTypeId int null --new
 			, TitleIStatusId int null --new
 			, TitleIIIStatusId int null --new
-			, DurationOfDisciplinaryAction int null --new
+			, DurationOfDisciplinaryAction decimal(18,2) null
 			
 		)
 		INSERT INTO #Facts
 		SELECT 
-			  sd.Id											StagingId
-			, rda.DimAgeId									AgeId
-			, rsy.DimSchoolYearId							SchoolYearId
-			, ISNULL(rdkd.DimK12DemographicId, -1)			K12DemographicId
-			, ISNULL(rddisc.DimDisciplineStatusId, -1)		DisciplineId
-			, @FactTypeId									FactTypeId
-			, ISNULL(rdis.DimIdeaStatusId, -1)				IdeaStatusId
-			, ISNULL(rdksch.DimK12SchoolId, -1)				K12SchoolId
-			, rdp.DimPersonId								K12StudentId
-			, 1												DisciplineCount
-		--	, ISNULL(sd.DurationOfDisciplinaryAction, 0)	DisciplineDuration
-			, ISNULL(rdf.DimFirearmId, -1)					FirearmId
-			, ISNULL(rgls.DimGradeLevelId, -1)				GradeLevelId
-			, -1											CteStatusId
-			, ISNULL(rdl.DimLeaID, -1)						LeaId
-			, ISNULL(rdr.DimRaceId, -1)						RaceId
-			, ISNULL(rds.DimSeaId, -1)						SeaId
-			, -1											IeuId
-			, -1											DataCollectionId
-			, -1											DisabilityStatusId
-			, -1											DisciplinaryActionEndDateId  
-			, ISNULL(disaction.DimDateId, -1)				DisciplinaryActionStartDateId 
-			, ISNULL(rddisc.DimDisciplineStatusId, -1)		DisciplineStatusId
-			, -1											EconomicallyDisadvantagedStatusId 
-			, ISNULL(rdels.DimEnglishLearnerStatusId, -1)	EnglishLearnerStatusId
-			, ISNULL(rdfds.DimFirearmDisciplineStatusId, -1)FirearmDisciplineStatusId 
-			, -1											FosterCareStatusId
-			, -1											HomelessnessStatusId
-			, -1											ImmigrantStatusId
-			, sd.IncidentIdentifier							IncidentIdentifier
-			, -1											IncidentStatusId
-			, ISNULL(Incident.DimDateId, -1)				IncidentDateId 
-			, -1											MigrantStatusId
-			, -1											MilitaryStatusId
-			, -1											NOrDStatusId
-			, ISNULL(rdidt.DimIdeaDisabilityTypeId, -1)		PrimaryDisabilityTypeId
-			, -1											SecondaryDisabilityTypeId
-			, -1											TitleIStatusId
-			, -1											TitleIIIStatusId
-			, ISNULL(sd.DurationOfDisciplinaryAction, 0)	DurationOfDisciplinaryAction
+			sd.Id												StagingId
+			, rda.DimAgeId										AgeId
+			, rsy.DimSchoolYearId								SchoolYearId
+			, ISNULL(rdkd.DimK12DemographicId, -1)				K12DemographicId
+			, ISNULL(rddisc.DimDisciplineStatusId, -1)			DisciplineId
+			, @FactTypeId										FactTypeId
+			, ISNULL(rdis.DimIdeaStatusId, -1)					IdeaStatusId
+			, ISNULL(rdksch.DimK12SchoolId, -1)					K12SchoolId
+			, rdp.DimPersonId									K12StudentId
+			, 1													DisciplineCount
+			, ISNULL(rdf.DimFirearmId, -1)						FirearmId
+			, ISNULL(rgls.DimGradeLevelId, -1)					GradeLevelId
+			, -1												CteStatusId
+			, ISNULL(rdl.DimLeaID, -1)							LeaId
+			, ISNULL(rdr.DimRaceId, -1)							RaceId
+			, ISNULL(rds.DimSeaId, -1)							SeaId
+			, -1												IeuId
+			, -1												DataCollectionId
+			, -1												DisabilityStatusId
+			, -1												DisciplinaryActionEndDateId  
+			, ISNULL(disaction.DimDateId, -1)					DisciplinaryActionStartDateId 
+			, ISNULL(rddisc.DimDisciplineStatusId, -1)			DisciplineStatusId
+			, -1												EconomicallyDisadvantagedStatusId 
+			, ISNULL(rdels.DimEnglishLearnerStatusId, -1)		EnglishLearnerStatusId
+			, ISNULL(rdfds.DimFirearmDisciplineStatusId, -1)	FirearmDisciplineStatusId 
+			, -1												FosterCareStatusId
+			, -1												HomelessnessStatusId
+			, ISNULL(rdidt.DimIdeaDisabilityTypeId, -1)			IdeaDisabilityTypeId
+			, -1												ImmigrantStatusId
+			, sd.IncidentIdentifier								IncidentIdentifier
+			, -1												IncidentStatusId
+			, ISNULL(Incident.DimDateId, -1)					IncidentDateId 
+			, -1												MigrantStatusId
+			, -1												MilitaryStatusId
+			, -1												NOrDStatusId
+			, ISNULL(rdidt.DimIdeaDisabilityTypeId, -1)			PrimaryDisabilityTypeId
+			, -1												SecondaryDisabilityTypeId
+			, -1												TitleIStatusId
+			, -1												TitleIIIStatusId
+			, ISNULL(sd.DurationOfDisciplinaryAction, 0)		DurationOfDisciplinaryAction
 		FROM Staging.Discipline sd 
 			JOIN Staging.K12Enrollment ske
 				ON sd.StudentIdentifierState = ske.StudentIdentifierState
 				AND ISNULL(sd.LeaIdentifierSeaAccountability, '') = ISNULL(ske.LeaIdentifierSeaAccountability, '')
 				AND ISNULL(sd.SchoolIdentifierSea, '') = ISNULL(ske.SchoolIdentifierSea, '')
 				AND sd.DisciplinaryActionStartDate BETWEEN ske.EnrollmentEntryDate AND ISNULL(ske.EnrollmentExitDate, @EndDate)
-
+ 
 			--program participation special education	
 			JOIN Staging.ProgramParticipationSpecialEducation sppse
 				ON ske.StudentIdentifierState = sppse.StudentIdentifierState
@@ -248,11 +248,11 @@ BEGIN
 		
 			JOIN RDS.DimSchoolYears rsy
 				ON ske.SchoolYear = rsy.SchoolYear
-
+ 
 			--age
 			JOIN RDS.DimAges rda
 				ON RDS.Get_Age(ske.Birthdate, @ChildCountDate) = rda.AgeValue
-
+ 
 			--dimpeople	(rds)
 			JOIN RDS.DimPeople rdp
 				ON ske.StudentIdentifierState = rdp.K12StudentStudentIdentifierState
@@ -267,7 +267,7 @@ BEGIN
 			--seas (rds)			
 			JOIN RDS.DimSeas rds
 				ON sd.DisciplinaryActionStartDate BETWEEN rds.RecordStartDateTime AND ISNULL(rds.RecordEndDateTime, GETDATE())	
-
+ 
 			--leas (rds)
 			LEFT JOIN RDS.DimLeas rdl
 				ON sd.LeaIdentifierSeaAccountability = rdl.LeaIdentifierSea
@@ -284,7 +284,7 @@ BEGIN
 				AND ISNULL(ske.Sex, 'MISSING') = ISNULL(rdkd.SexMap, rdkd.SexCode)
 			
 			-- disciplineId
-			LEFT JOIN #vwDisciplinesStatuses rddisc
+			LEFT JOIN #vwDisciplineStatuses rddisc
 				ON rsy.SchoolYear = rddisc.SchoolYear
 				AND ISNULL(sd.DisciplinaryActionTaken, 'MISSING')						= ISNULL(rddisc.DisciplinaryActionTakenMap, rddisc.DisciplinaryActionTakenCode)
 				AND ISNULL(sd.DisciplineMethodOfCwd, 'MISSING')							= ISNULL(rddisc.DisciplineMethodOfChildrenWithDisabilitiesMap, rddisc.DisciplineMethodOfChildrenWithDisabilitiesCode)
@@ -301,8 +301,8 @@ BEGIN
 			
 			--idea disability type (rds)
 			LEFT JOIN RDS.vwDimIdeaDisabilityTypes rdidt
-					ON ISNULL(sidt.IdeaDisabilityTypeCode, 'MISSING') = ISNULL(rdidt.IdeaDisabilityTypeMap, rdidt.IdeaDisabilityTypeCode)
-					AND sidt.IsPrimaryDisability = 1
+				ON ISNULL(sidt.IdeaDisabilityTypeCode, 'MISSING') = ISNULL(rdidt.IdeaDisabilityTypeMap, rdidt.IdeaDisabilityTypeCode)
+				AND sidt.IsPrimaryDisability = 1
 			
 			--grade levels (rds)
 			LEFT JOIN #vwGradeLevels rgls
@@ -316,7 +316,7 @@ BEGIN
 				AND ske.StudentIdentifierState = spr.StudentIdentifierState
 				AND ISNULL(ske.LEAIdentifierSeaAccountability,'') = ISNULL(spr.LeaIdentifierSeaAccountability,'')
 				AND ISNULL(ske.SchoolIdentifierSea,'') = ISNULL(spr.SchoolIdentifierSea,'')
-
+ 
 			LEFT JOIN #vwRaces rdr
 				ON ISNULL(rdr.RaceMap, rdr.RaceCode) =
 					CASE
@@ -332,15 +332,15 @@ BEGIN
 				AND ISNULL(CAST(el.EnglishLearnerStatus AS SMALLINT), -1) = ISNULL(rdels.EnglishLearnerStatusMap, -1)
 		
 			--firearm disciplines (rds)
-			LEFT JOIN RDS.vwDimFirearmDisciplinesStatuses rdfds
-					ON rsy.SchoolYear = rdis.SchoolYear				
-					AND ISNULL(sd.DisciplineMethodFirearm, 'MISSING')		= ISNULL(rdfds.DisciplineMethodForFirearmsIncidentsMap, rdfds.DisciplineMethodForFirearmsIncidentsCode)
-					AND ISNULL(sd.IDEADisciplineMethodFirearm, 'MISSING')					= ISNULL(rdfds.IdeaDisciplineMethodForFirearmsIncidentsMap, rdfds.IdeaDisciplineMethodForFirearmsIncidentsCode) 
+			LEFT JOIN RDS.vwDimFirearmDisciplineStatuses rdfds
+				ON rsy.SchoolYear = rdis.SchoolYear				
+				AND ISNULL(sd.DisciplineMethodFirearm, 'MISSING')		= ISNULL(rdfds.DisciplineMethodForFirearmsIncidentsMap, rdfds.DisciplineMethodForFirearmsIncidentsCode)
+				AND ISNULL(sd.IDEADisciplineMethodFirearm, 'MISSING')	= ISNULL(rdfds.IdeaDisciplineMethodForFirearmsIncidentsMap, rdfds.IdeaDisciplineMethodForFirearmsIncidentsCode) 
 	
 			-- incident date
 			LEFT JOIN RDS.DimDates incident 
 				ON sd.IncidentDate =  incident.DateValue
-
+ 
 			-- disciplinary action start date
 			LEFT JOIN RDS.DimDates disaction 
 				ON sd.DisciplinaryActionStartDate =  disaction.DateValue
@@ -349,9 +349,9 @@ BEGIN
 			LEFT JOIN vwDimFirearms rdf
 				ON rsy.SchoolYear = rdf.SchoolYear				
 				AND ISNULL(sd.FirearmType, 'MISSING')		= ISNULL(rdf.FirearmTypeMap, rdf.FirearmTypeCode)
-
+ 
 		--IF EXISTS (SELECT 1 FROM Staging.ProgramParticipationCTE) BEGIN
-
+ 
 		--	IF OBJECT_ID('tempdb.dbo.#vwDimCteStatuses', 'U') IS NOT NULL 
 		--		DROP TABLE #vwDimCteStatuses; 		
 		--	SELECT v.* INTO #vwDimCteStatuses FROM RDS.vwDimCteStatuses v
@@ -398,95 +398,95 @@ BEGIN
 		--		AND ISNULL(CAST(sppc_sp.SingleParentIndicator AS SMALLINT), -1)					= ISNULL(rdcs.SingleParentOrSinglePregnantWomanMap, -1)
 		--		AND ISNULL(CAST(sps.PerkinsLEPStatus AS SMALLINT), -1)							= ISNULL(rdcs.LepPerkinsStatusMap, -1)
 		--END
-
+ 
 		/*  Final insert into RDS.FactK12StudentDisciplines  table */
 		INSERT INTO RDS.FactK12StudentDisciplines (
-		--add more fields
-			  SchoolYearId
-			  ,FactTypeId
-			  ,DataCollectionId
-			  ,SeaId
-			  ,IeuId
-			  ,LeaId
-			  ,K12SchoolId
-			  ,K12StudentId
-			  ,AgeId
-			  ,CteStatusId
-			  ,DisabilityStatusId
-			  ,DisciplinaryActionStartDateId
-			  ,DisciplinaryActionEndDateId
-			  ,DisciplineStatusId
-			  ,EconomicallyDisadvantagedStatusId
-			  ,EnglishLearnerStatusId
-			  ,FirearmId
-			  ,FirearmDisciplineStatusId
-			  ,FosterCareStatusId
-			  ,GradeLevelId
-			  ,HomelessnessStatusId
-			  ,IdeaStatusId
-			  ,ImmigrantStatusId
-			  ,IncidentIdentifier
-			  ,IncidentStatusId
-			  ,IncidentDateId
-			  ,K12DemographicId
-			  ,MigrantStatusId
-			  ,MilitaryStatusId
-			  ,NOrDStatusId
-			  ,RaceId
-			  ,PrimaryDisabilityTypeId
-			  ,SecondaryDisabilityTypeId
-			  ,TitleIStatusId
-			  ,TitleIIIStatusId
-			  ,DurationOfDisciplinaryAction
-			  ,DisciplineCount
-
+			SchoolYearId
+			,FactTypeId
+			,DataCollectionId
+			,SeaId
+			,IeuId
+			,LeaId
+			,K12SchoolId
+			,K12StudentId
+			,AgeId
+			,CteStatusId
+			,DisabilityStatusId
+			,DisciplinaryActionStartDateId
+			,DisciplinaryActionEndDateId
+			,DisciplineStatusId
+			,EconomicallyDisadvantagedStatusId
+			,EnglishLearnerStatusId
+			,FirearmId
+			,FirearmDisciplineStatusId
+			,FosterCareStatusId
+			,GradeLevelId
+			,HomelessnessStatusId
+			,IdeaStatusId
+			,IdeaDisabilityTypeId
+			,ImmigrantStatusId
+			,IncidentIdentifier
+			,IncidentStatusId
+			,IncidentDateId
+			,K12DemographicId
+			,MigrantStatusId
+			,MilitaryStatusId
+			,NOrDStatusId
+			,RaceId
+			,PrimaryDisabilityTypeId
+			,SecondaryDisabilityTypeId
+			,TitleIStatusId
+			,TitleIIIStatusId
+			,DurationOfDisciplinaryAction
+			,DisciplineCount
+ 
 		)
 		SELECT 
 			SchoolYearId
-			  ,FactTypeId
-			  ,DataCollectionId
-			  ,SeaId
-			  ,IeuId
-			  ,LeaId
-			  ,K12SchoolId
-			  ,K12StudentId
-			  ,AgeId
-			  ,CteStatusId
-			  ,DisabilityStatusId
-			  ,DisciplinaryActionStartDateId
-			  ,DisciplinaryActionEndDateId
-			  ,DisciplineStatusId
-			  ,EconomicallyDisadvantagedStatusId
-			  ,EnglishLearnerStatusId
-			  ,FirearmId
-			  ,FirearmDisciplineStatusId
-			  ,FosterCareStatusId
-			  ,GradeLevelId
-			  ,HomelessnessStatusId
-			  ,IdeaStatusId
-			  ,ImmigrantStatusId
-			  ,IncidentIdentifier
-			  ,IncidentStatusId
-			  ,IncidentDateId
-			  ,K12DemographicId
-			  ,MigrantStatusId
-			  ,MilitaryStatusId
-			  ,NOrDStatusId
-			  ,RaceId
-			  ,PrimaryDisabilityTypeId
-			  ,SecondaryDisabilityTypeId
-			  ,TitleIStatusId
-			  ,TitleIIIStatusId
-			  ,DurationOfDisciplinaryAction
-			  ,DisciplineCount
+			,FactTypeId
+			,DataCollectionId
+			,SeaId
+			,IeuId
+			,LeaId
+			,K12SchoolId
+			,K12StudentId
+			,AgeId
+			,CteStatusId
+			,DisabilityStatusId
+			,DisciplinaryActionStartDateId
+			,DisciplinaryActionEndDateId
+			,DisciplineStatusId
+			,EconomicallyDisadvantagedStatusId
+			,EnglishLearnerStatusId
+			,FirearmId
+			,FirearmDisciplineStatusId
+			,FosterCareStatusId
+			,GradeLevelId
+			,HomelessnessStatusId
+			,IdeaStatusId
+			,IdeaDisabilityTypeId
+			,ImmigrantStatusId
+			,IncidentIdentifier
+			,IncidentStatusId
+			,IncidentDateId
+			,K12DemographicId
+			,MigrantStatusId
+			,MilitaryStatusId
+			,NOrDStatusId
+			,RaceId
+			,PrimaryDisabilityTypeId
+			,SecondaryDisabilityTypeId
+			,TitleIStatusId
+			,TitleIIIStatusId
+			,DurationOfDisciplinaryAction
+			,DisciplineCount
 		FROM #Facts
-
-	ALTER INDEX ALL ON RDS.FactK12StudentCounts REBUILD
-		
+ 
+		ALTER INDEX ALL ON RDS.FactK12StudentCounts REBUILD
 	
 	END TRY
 	BEGIN CATCH
 		INSERT INTO Staging.ValidationErrors VALUES ('Staging.Staging-to-FactK12StudentDisciplines', 'RDS.FactK12StudentDisciplines', 'FactK12StudentDisciplines', NULL, ERROR_MESSAGE(), 1, NULL, GETDATE())
 	END CATCH
-
+ 
 END
