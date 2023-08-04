a0813d8ea3bc11440dec5fe3ed9e1dbc2110c870
johnny cunningham <jcunningham@aemcorp.com>
2023-08-03 20:18:12 -0400

    committing corrected version of new script

--- generate.database/VersionUpdates/11.0_prerelease/CreateSourceToStagingShellStoredProcedures.sql
+++ generate.database/VersionUpdates/11.0_prerelease/CreateSourceToStagingShellStoredProcedures.sql
@@ -1,140 +1,377 @@
 set nocount on
 begin try
-	begin transaction
+begin transaction
 
-		--Create the new schema to house the Source to Staging scripts
-		IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Source')
-		BEGIN
-			EXEC('CREATE SCHEMA [Source]')
-		END
+--Create the new schema to house the Source to Staging scripts
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Source')
+BEGIN
+	EXEC('CREATE SCHEMA [Source]')
+END
 
-		--Check for the existance of the Source to Staging scripts in the new name format
-		--	remove them so they can all be added in the correct order later in this process
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Directory%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Directory%'
-		END
+---------------------------------------------------------------------------------------------------
+--Create the new stored procedures for the Source to Staging scripts if they don't already exist
+---------------------------------------------------------------------------------------------------
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_ChildCount%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_ChildCount%'
-		END
+--Directory
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Directory'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Directory] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Exiting%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Exiting%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Discipline%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Discipline%'
-		END
+--Child Count
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_ChildCount'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_ChildCount] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Assessments%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Assessments%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Staff%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Staff%'
-		END
+--Exiting
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Exiting'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Exiting] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_ChronicAbsenteeism%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_ChronicAbsenteeism%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Dropout%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Dropout%'
-		END
+--Discipline
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Discipline'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Discipline] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_GraduatesCompleters%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_GraduatesCompleters%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_GraduationRate%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_GraduationRate%'
-		END
+--Assessments
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Assessments'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Assessments] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Homeless%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Homeless%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_HSGradPSEnroll%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_HSGradPSEnroll%'
-		END
+--Staff
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Staff'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Staff] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Immigrant%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Immigrant%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Membership%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_Membership%'
-		END
+--Chronic Absenteeism
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_ChronicAbsenteeism'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_ChronicAbsenteeism] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_MigrantEdProgram%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_MigrantEdProgram%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_NeglectedOrDelinquent%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_NeglectedOrDelinquent%'
-		END
+--Dropout
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Dropout'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Dropout] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleI%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleI%'
-		END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleIIIELOct%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleIIIELOct%'
-		END
+--Graduates Completers
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_GraduatesCompleters'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_GraduatesCompleters] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		IF EXISTS (SELECT 1 FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleIIIELSY%')
-		BEGIN
-			DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like '%Source-to-Staging_TitleIIIELSY%'
-		END
 
-		--Clear the old IDS migration scripts
-		DELETE FROM app.DataMigrationTasks WHERE StoredProcedureName like 'Staging.Wrapper_Migrate_%_to_IDS%'
+--Graduation Rate
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_GraduationRate'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_GraduationRate] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
 
-		--Create space in the table for the new procdures 
-		update app.DataMigrationTasks
-		set TaskSequence = TaskSequence + 20
-		where DataMigrationTypeId = 1 
 
-		--Add the new stored procedures
-		insert into app.DataMigrationTasks
-		values
-		(1,1,0,0,'Source.Source-to-Staging_Directory @SchoolYear',				1,	0,'029, 035, 039, 103, 129, 130, 131, 163, 170, 190, 193, 196, 197, 198, 205, 206',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_ChildCount @SchoolYear',				2,	0,'002, 089',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Exiting @SchoolYear',				3,	0,'009',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Discipline @SchoolYear',				4,	0,'005, 006, 007, 086, 088, 143, 144',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Staff @SchoolYear',					5,	0,'059, 065, 067, 070, 099, 112, 203',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Assessment @SchoolYear',				6,	0,'113, 125, 126, 137, 138, 139, 142, 175, 178, 179, 185, 188, 189',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_ChronicAbsenteeism @SchoolYear',		7,	0,'195',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Dropout @SchoolYear',				8,	0,'032',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_GraduatesCompleters @SchoolYear',	9,	0,'040',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_GraduationRate @SchoolYear',			10,	0,'150, 151',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Homeless @SchoolYear',				11,	0,'118, 194',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_HsGradPsEnroll @SchoolYear',			12,	0,'160',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Immigrant @SchoolYear',				13,	0,'165',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_Membership @SchoolYear',				14,	0,'033, 052',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_MigrantEdProgram @SchoolYear',		15,	0,'054, 121, 145',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_NeglectedOrDelinquent @SchoolYear',	16,	0,'119, 127',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_TitleI @SchoolYear',					17,	0,'037, 134',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_TitleIIIELOct @SchoolYear',			18,	0,'141',NULL),
-		(1,1,0,0,'Source.Source-to-Staging_TitleIIIELSY @SchoolYear',			19,	0,'045, 116',NULL)
+--Homeless
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Homeless'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Homeless] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--HS Grad PS Enroll
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_HSGradPSEnroll'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_HSGradPSEnroll] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Immigrant
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Immigrant'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Immigrant] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Membership
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_Membership'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_Membership] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Migrant Education Program
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_MigrantEdProgram'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_MigrantEdProgram] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Neglected or Delinquent
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_NeglectedOrDelinquent'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_NeglectedOrDelinquent] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Title I
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_TitleI'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_TitleI] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Title III EL Oct
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_TitleIIIELOct'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_TitleIIIELOct] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
+
+--Title III EL SY
+IF NOT EXISTS (select *
+				from sys.procedures p 
+					join sys.schemas s
+						on p.schema_id = s.schema_id
+				where s.name = 'Source'
+				and p.name = 'Source-to-Staging_TitleIIIELSY'
+)
+BEGIN
+	EXEC ('CREATE PROCEDURE [Source].[Source-to-Staging_TitleIIIELSY] 
+	@schoolYear smallint
+AS
+--BEGIN
+	--State specific ETL code here
+--END
+')
+END
+
 
 
 	commit transaction
