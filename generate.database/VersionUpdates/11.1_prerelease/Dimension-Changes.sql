885584c56d48e53630f6f30a3265400d99bb9f6c
Sumanth Vuyuru <sumanth.vuyuru@aemcorp.com>
2023-08-28 17:58:02 -0400

    CIID-5704 : Assessment related changes.

---
+++ generate.database/VersionUpdates/11.1_prerelease/Dimension-Changes.sql
@@ -0,0 +1,49 @@
+SET NOCOUNT ON;
+
+DECLARE @dimensionTableId as INT, @categoryId as INT, @dimensionId as INT
+
+SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'GRADELEVEL'
+SELECT @categoryId = CategoryId FROM app.Categories WHERE CategoryCode = 'GRADELVLLG'
+
+IF NOT EXISTS(SELECT 1 from app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]([CategoryId],[DimensionId])
+    VALUES (@categoryId, @dimensionId)
+END
+
+
+SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'GRADELEVEL'
+SELECT @categoryId = CategoryId FROM app.Categories WHERE CategoryCode = 'GRADELVLHS'
+
+IF NOT EXISTS(SELECT 1 from app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]([CategoryId],[DimensionId])
+    VALUES (@categoryId, @dimensionId)
+END
+
+SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'AssessmentTypeAdministered'
+SELECT @categoryId = CategoryId FROM app.Categories WHERE CategoryCode = 'ASMTADMNMTHLG'
+
+IF NOT EXISTS(SELECT 1 from app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]([CategoryId],[DimensionId])
+    VALUES (@categoryId, @dimensionId)
+END
+
+SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'AssessmentTypeAdministered'
+SELECT @categoryId = CategoryId FROM app.Categories WHERE CategoryCode = 'ASMTADMNMTHHS'
+
+IF NOT EXISTS(SELECT 1 from app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]([CategoryId],[DimensionId])
+    VALUES (@categoryId, @dimensionId)
+END
+
+SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'AssessmentTypeAdministered'
+SELECT @categoryId = CategoryId FROM app.Categories WHERE CategoryCode = 'ASMTADMNMTHLG'
+
+IF NOT EXISTS(SELECT 1 from app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]([CategoryId],[DimensionId])
+    VALUES (@categoryId, @dimensionId)
+END
