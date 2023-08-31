fe341bfd133059b1d5bde38898061fdd7a6b6350
Sumanth Vuyuru <sumanth.vuyuru@aemcorp.com>
2023-08-31 13:30:52 -0400

    CIID-5704 : Assessment related changes.

--- generate.database/RunDatabaseScripts.ps1
+++ generate.database/RunDatabaseScripts.ps1
@@ -1,5 +1,5 @@
 Param(
-  [string] $version = "11.0_prerelease",
+  [string] $version = "11.1_prerelease",
   [string] $sqlServer = "(localdb)\MSSQLLocalDB",
   [string] $db = "generate",
   [string] $user,
