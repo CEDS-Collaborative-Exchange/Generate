23cc385fd20157c009e6f1305492ecc718830df8
johnnycunningham <jcunningham@aemcorp.com>
2023-10-27 14:41:51 -0400

    fix the drop

--- generate.database/Views/Drop/Debug.vwAssessments_FactTable.View.sql
+++ generate.database/Views/Drop/Debug.vwAssessments_FactTable.View.sql
@@ -1,2 +1,2 @@
-IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[Debug].[vwAssessment_FactTable]'))
-DROP VIEW [Debug].[vwAssessment_FactTable]
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[Debug].[vwAssessments_FactTable]'))
+DROP VIEW [Debug].[vwAssessments_FactTable]
