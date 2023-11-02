16f67d355b8f3a3b91d63bc8e7a6542484349807
johnnycunningham <jcunningham@aemcorp.com>
2023-11-02 10:07:21 -0400

    remove the SY condition from the cleanup

--- generate.database/VersionUpdates/11.2_prerelease/Update File Submission File Description.sql
+++ generate.database/VersionUpdates/11.2_prerelease/Update File Submission File Description.sql
@@ -1,4 +1,3 @@
 --Use the function to remove the numeric values in the FileSubmissionDescription column
 UPDATE app.FileSubmissions
 SET FileSubmissionDescription = dbo.fn_StripCharacters(FileSubmissionDescription, '^ a-z4-9')
-WHERE SubmissionYear = 2023
