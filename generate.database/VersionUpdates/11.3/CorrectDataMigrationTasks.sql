85529f536bb88ac6ff5211a5c7ce8e62f388ea05
johnnycunningham <jcunningham@aemcorp.com>
2024-02-29 10:08:15 -0500

    Correct DataMigrationTasks for the 11.3 release

--- generate.database/VersionUpdates/11.3/CorrectDataMigrationTasks.sql
+++ generate.database/VersionUpdates/11.3/CorrectDataMigrationTasks.sql
@@ -8,3 +8,34 @@ update app.DataMigrationTasks
 set StoredProcedureName = replace(StoredProcedureName,' @SchoolYear', '] @SchoolYear')
 where DataMigrationTypeId = 1
 and StoredProcedureName not like '%] @SchoolYear%'
+ 
+--update the StoredProcedureName for the wrappers to use the changed names
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_GraduatesCompleters_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_Grad_to_RDS'
+ 
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_GraduationRate_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_GradRate_to_RDS'
+ 
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_HsGradPSEnroll_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_HsGradEnroll_to_RDS'
+ 
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_MigrantEducationProgram_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_Mep_to_RDS'
+ 
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_NeglectedOrDelinquent_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_NorD_to_RDS'
+ 
+update app.DataMigrationTasks
+set StoredProcedureName = 'App.Wrapper_Migrate_Staff_to_RDS'
+where DataMigrationTypeId = 2
+and StoredProcedureName = 'App.Wrapper_Migrate_Personnel_to_RDS'
