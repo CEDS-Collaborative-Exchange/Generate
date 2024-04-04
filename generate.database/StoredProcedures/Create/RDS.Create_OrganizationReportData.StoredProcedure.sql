99278a8e7495427a78f4d7f9baae0460c084db8d
johnnycunningham <jcunningham@aemcorp.com>
2024-04-04 16:54:57 -0400

    Fix 039 issue

--- generate.database/StoredProcedures/Create/RDS.Create_OrganizationReportData.StoredProcedure.sql
+++ generate.database/StoredProcedures/Create/RDS.Create_OrganizationReportData.StoredProcedure.sql
@@ -571,6 +571,7 @@ BEGIN
 								on grades.DimGradeLevelId = bridgeGrades.GradeLevelId
 						where d.SchoolYear = @reportYear 
 						and sch.DimK12SchoolId <> -1 
+						and sch.SchoolTypeEdFactsCode <> '5' -- Exclude Reportable Programs, requirement added for SY 23-24
 						and ISNULL(sch.ReportedFederally, 1) = 1
 						and sch.SchoolOperationalStatus not in ('Closed', 'FutureSchool', 'Inactive', 'MISSING')
 					end
@@ -1799,7 +1800,8 @@ BEGIN
 								left outer join rds.DimGradeLevels grades 
 									on grades.DimGradeLevelId = bridgeGrades.GradeLevelId
 							where dates.SchoolYear = @reportYear 
-							and sch.DimK12SchoolId <> -1 
+							and sch.DimK12SchoolId <> -1
+							and sch.SchoolTypeEdFactsCode <> '5' -- Exclude Reportable Programs, requirement added for SY 23-24
 							and ISNULL(sch.ReportedFederally, 1) = 1
 							and sch.SchoolOperationalStatus not in ('Closed', 'FutureSchool', 'Inactive', 'MISSING')
 						end
