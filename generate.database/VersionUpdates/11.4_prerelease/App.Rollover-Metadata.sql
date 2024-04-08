761419e5e2d044c11b4e3b50320b247f5f2f649d
Sumanth Vuyuru <svuyuru@aemcorp.com>
2024-04-08 10:31:42 -0400

    CIID-6499 : Rollover Metadata.

---
+++ generate.database/VersionUpdates/11.4_prerelease/App.Rollover-Metadata.sql
@@ -0,0 +1,69 @@
+SET NOCOUNT ON;
+
+EXEC App.Rollover_Previous_Year_Metadata 'c129' , 2022, 2023
+EXEC App.Rollover_Previous_Year_Metadata 'c130' , 2022, 2023
+EXEC App.Rollover_Previous_Year_Metadata 'c197' , 2022, 2023
+EXEC App.Rollover_Previous_Year_Metadata 'c198' , 2022, 2023
+EXEC App.Rollover_Previous_Year_Metadata 'c207' , 2022, 2023
+EXEC App.Rollover_Previous_Year_Metadata 'c211' , 2022, 2023
+
+
+-- Delete all 2024 metadata
+delete co
+from app.CategorySets cs 
+inner join app.CategorySet_Categories csc on cs.CategorySetId = csc.CategorySetId
+inner join app.Categories c on csc.CategoryId = c.CategoryId
+inner join app.CategoryOptions co on cs.CategorySetId = co.CategorySetId and co.CategoryId = c.CategoryId
+where SubmissionYear = 2024
+
+delete csc
+from app.CategorySets cs 
+inner join app.CategorySet_Categories csc on cs.CategorySetId = csc.CategorySetId
+inner join app.Categories c on csc.CategoryId = c.CategoryId
+where SubmissionYear = 2024
+
+delete from app.CategorySets where SubmissionYear = 2024
+
+delete fsfc
+from app.FileSubmission_FileColumns fsfc
+inner join app.FileSubmissions fs on fs.FileSubmissionId = fsfc.FileSubmissionId
+where fs.SubmissionYear = '2024'
+
+delete from app.FileSubmissions where SubmissionYear = '2024'
+
+DECLARE @reportCode VARCHAR(5)
+
+DECLARE csy CURSOR FOR
+        SELECT distinct agr.ReportCode
+        FROM App.FileSubmissions fs
+		JOIN App.GenerateReports agr
+			ON fs.GenerateReportId = agr.GenerateReportId
+        WHERE SubmissionYear = 2023
+            AND ReportCode IN (
+                 'c002','c029'
+                ,'c033','c035','c039','c045'
+				,'c050','c052','c054','c059'
+				,'c067','c089','c116','c118'
+				,'c121','c126','c129'
+				,'c130','c137','c138','c139'
+				,'c141','c145','c165'
+				,'c170','c175','c178','c179'
+				,'c185','c188','c189'
+				,'c190','c194','c195','c196','c197','c198'
+				,'c203','c207','c210','c211'
+            )
+        ORDER BY agr.ReportCode
+
+    SET @reportCode = NULL
+    OPEN csy
+    FETCH NEXT FROM csy INTO @reportCode
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        EXEC App.Rollover_Previous_Year_Metadata @reportCode , 2023, 2024
+    
+        FETCH NEXT FROM csy INTO @reportCode
+    END
+
+    CLOSE csy
+    DEALLOCATE csy
