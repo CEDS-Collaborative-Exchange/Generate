0dea055973bb25b510d7d9e1ba2ad84b87dcad32
jwheaton2963 <jwheaton@msfw.com>
2024-04-10 15:04:20 -0500

    Added code to log when test produced no results

--- generate.database/TestCases/App.FS033_TestCase.StoredProcedure.sql
+++ generate.database/TestCases/App.FS033_TestCase.StoredProcedure.sql
@@ -248,5 +248,30 @@ BEGIN
 
 		DROP TABLE #SCH_TOT
 
-	
+-- IF THE TEST PRODUCES NO RESULTS INSERT A RECORD TO INDICATE THIS -------------------------
+if not exists(select top 1 * from app.sqlunittest t
+	inner join app.SqlUnitTestCaseResult r
+		on t.SqlUnitTestId = r.SqlUnitTestId
+		and t.SqlUnitTestId = @SqlUnitTestId)
+begin
+			INSERT INTO App.SqlUnitTestCaseResult 
+			(
+				[SqlUnitTestId]
+				,[TestCaseName]
+				,[TestCaseDetails]
+				,[ExpectedResult]
+				,[ActualResult]
+				,[Passed]
+				,[TestDateTime]
+			)
+			SELECT DISTINCT
+				 @SqlUnitTestId
+				,'NO TEST RESULTS'
+				,'NO TEST RESULTS'
+				,-1
+				,-1
+				,-1
+				,GETDATE()
+end
+----------------------------------------------------------------------------------	
 END
