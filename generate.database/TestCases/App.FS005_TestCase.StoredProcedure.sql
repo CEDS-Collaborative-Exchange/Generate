5d011c29e12d084e5a18a6292655b14973774c05
AEMJohnnyCunningham <62306220+AEMJohnnyCunningham@users.noreply.github.com>
2024-04-11 19:22:28 -0400

    Update App.FS005_TestCase.StoredProcedure.sql

--- generate.database/TestCases/App.FS005_TestCase.StoredProcedure.sql
+++ generate.database/TestCases/App.FS005_TestCase.StoredProcedure.sql
@@ -730,34 +730,6 @@ BEGIN
 		AND rreksd.CategorySetCode = 'ST1'
 			
 	DROP TABLE #L_ST1
-<<<<<<< HEAD
--- IF THE TEST PRODUCES NO RESULTS INSERT A RECORD TO INDICATE THIS -------------------------
-if not exists(select top 1 * from app.sqlunittest t
-	inner join app.SqlUnitTestCaseResult r
-		on t.SqlUnitTestId = r.SqlUnitTestId
-		and t.SqlUnitTestId = @SqlUnitTestId)
-begin
-			INSERT INTO App.SqlUnitTestCaseResult 
-			(
-				[SqlUnitTestId]
-				,[TestCaseName]
-				,[TestCaseDetails]
-				,[ExpectedResult]
-				,[ActualResult]
-				,[Passed]
-				,[TestDateTime]
-			)
-			SELECT DISTINCT
-				 @SqlUnitTestId
-				,'NO TEST RESULTS'
-				,'NO TEST RESULTS'
-				,-1
-				,-1
-				,NULL
-				,GETDATE()
-end
-----------------------------------------------------------------------------------
-=======
 
 	-- IF THE TEST PRODUCES NO RESULTS INSERT A RECORD TO INDICATE THIS 
 	if not exists(select top 1 * from app.sqlunittest t
@@ -780,19 +752,18 @@ end
 			,'NO TEST RESULTS'
 			,-1
 			,-1
-			,-1
+			,0
 			,GETDATE()
 	end
 
->>>>>>> develop
 	--check the results
 
-	-- select *
-	-- from App.SqlUnitTestCaseResult sr
+	--select *
+	--from App.SqlUnitTestCaseResult sr
 	-- 	inner join App.SqlUnitTest s
 	-- 		on s.SqlUnitTestId = sr.SqlUnitTestId
-	-- where s.UnitTestName like '%005%'
-	-- and passed = 0
+	--where s.UnitTestName like '%005%'
+	--and passed = 0
 	--and convert(date, TestDateTime) = convert(date, GETDATE())
 
 END
