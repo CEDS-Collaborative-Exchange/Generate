d4c362ca493ea6e239d0ad43508b9078c8d28eb5
AEMJohnnyCunningham <62306220+AEMJohnnyCunningham@users.noreply.github.com>
2024-04-11 19:26:50 -0400

    Update App.FS088_TestCase.StoredProcedure.sql

--- generate.database/TestCases/App.FS088_TestCase.StoredProcedure.sql
+++ generate.database/TestCases/App.FS088_TestCase.StoredProcedure.sql
@@ -964,34 +964,33 @@ BEGIN
 			
 	DROP TABLE #L_TOT
 
--- IF THE TEST PRODUCES NO RESULTS INSERT A RECORD TO INDICATE THIS -------------------------
-if not exists(select top 1 * from app.sqlunittest t
-	inner join app.SqlUnitTestCaseResult r
-		on t.SqlUnitTestId = r.SqlUnitTestId
-		and t.SqlUnitTestId = @SqlUnitTestId)
-begin
-			INSERT INTO App.SqlUnitTestCaseResult 
-			(
-				[SqlUnitTestId]
-				,[TestCaseName]
-				,[TestCaseDetails]
-				,[ExpectedResult]
-				,[ActualResult]
-				,[Passed]
-				,[TestDateTime]
-			)
-			SELECT DISTINCT
-				 @SqlUnitTestId
-				,'NO TEST RESULTS'
-				,'NO TEST RESULTS'
-				,-1
-				,-1
-				,NULL
-				,GETDATE()
-end
-----------------------------------------------------------------------------------
-	--check the results
+	-- IF THE TEST PRODUCES NO RESULTS INSERT A RECORD TO INDICATE THIS
+	if not exists(select top 1 * from app.sqlunittest t
+		inner join app.SqlUnitTestCaseResult r
+			on t.SqlUnitTestId = r.SqlUnitTestId
+			and t.SqlUnitTestId = @SqlUnitTestId)
+	begin
+		INSERT INTO App.SqlUnitTestCaseResult 
+		(
+			[SqlUnitTestId]
+			,[TestCaseName]
+			,[TestCaseDetails]
+			,[ExpectedResult]
+			,[ActualResult]
+			,[Passed]
+			,[TestDateTime]
+		)
+		SELECT DISTINCT
+			 @SqlUnitTestId
+			,'NO TEST RESULTS'
+			,'NO TEST RESULTS'
+			,-1
+			,-1
+			,0
+			,GETDATE()
+	end
 
+	--check the results
 	--select *
 	--from App.SqlUnitTestCaseResult sr
 	--	inner join App.SqlUnitTest s
