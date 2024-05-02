6e7e6b3fe9ac793560685d6c9c0a7f23f88f594c
Weining Lu <Wlu@aemcorp.com>
2024-05-02 10:43:51 -0400

    Update test view for FS218

--- generate.database/Views/Create/Staging.vwNeglectedOrDelinquent_StagingTables_C218.View.sql
+++ generate.database/Views/Create/Staging.vwNeglectedOrDelinquent_StagingTables_C218.View.sql
@@ -15,14 +15,14 @@ AS
 	JOIN [Staging].[ProgramParticipationTitleI] pt 
 		on pt.LeaIdentifierSeaAccountability = vw.LEAIdentifierSeaAccountability
 		and pt.StudentIdentifierState = vw.StudentIdentifierState
-	JOIN [RDS].[DimLeas] lea on lea.LeaIdentifierSea = lea.SeaOrganizationIdentifierSea
+	JOIN [RDS].[DimLeas] lea on lea.LeaIdentifierSea = vw.LEAIdentifierSeaAccountability
 	LEFT JOIN excludedLeas el
 		ON vw.LEAIdentifierSeaAccountability = el.LeaIdentifierSea
 	WHERE el.LeaIdentifierSea IS NULL
 		AND NeglectedOrDelinquentAcademicOutcomeIndicator <> ''
 		AND TitleIIndicator IN ('01_1', '02_1', '03_1', '04_1', '05_1')
-		AND pt.ProgramParticipationBeginDate >= CAST(('7/1/' + CAST((vw.SchoolYear -1) as varchar))  AS Date)
-		AND CAST(ISNULL(pt.ProgramParticipationEndDate, '1900-01-01') AS DATE) <= CAST(('6/30/' + CAST(vw.SchoolYear as varchar))  AS Date)
+		AND vw.ProgramParticipationBeginDate >= CAST(('7/1/' + CAST((vw.SchoolYear -1) as varchar))  AS Date)
+		AND CAST(ISNULL(vw.ProgramParticipationEndDate, '1900-01-01') AS DATE) <= CAST(('6/30/' + CAST(vw.SchoolYear as varchar))  AS Date)
 	GROUP BY
 		 vw.StudentIdentifierState
 		,lea.SeaOrganizationIdentifierSea
