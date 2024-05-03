cc92640978cf4ade407eca5de14e46b47b4615aa
jwheaton2963 <jwheaton@msfw.com>
2024-05-03 09:27:33 -0500

    Another spelling.

--- generate.database/Views/Drop/App.CategoryCodeOptionsByReportAndYear.View.sql
+++ generate.database/Views/Drop/App.CategoryCodeOptionsByReportAndYear.View.sql
@@ -1,2 +1,2 @@
-IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[App].[CateogryCodeOptionsByReportAndYear]'))
-    DROP VIEW [App].[CateogryCodeOptionsByReportAndYear]
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[App].[CategoryCodeOptionsByReportAndYear]'))
+    DROP VIEW [App].[CategoryCodeOptionsByReportAndYear]
