b0106223fd456228e78362834dc3bc4b0dcce910
Sumanth Vuyuru <svuyuru@aemcorp.com>
2024-05-10 16:16:28 -0400

    CIID-5224 : Migration Database Changes.

--- generate.database/VersionUpdates/12.0_prerelease/App.TableChanges.sql
+++ generate.database/VersionUpdates/12.0_prerelease/App.TableChanges.sql
@@ -13,6 +13,31 @@ BEGIN
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 END
 
+IF NOT EXISTS(SELECT 1 FROM sys.indexes
+WHERE object_id = OBJECT_ID('App.GenerateReport_FactType') AND name = 'PK_GenerateReport_FactTypes')
+BEGIN
+	ALTER TABLE [App].[GenerateReport_FactType] ADD  CONSTRAINT [PK_GenerateReport_FactTypes] PRIMARY KEY CLUSTERED 
+	(
+		[GenerateReportId] ASC,
+		[FactTypeId] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+END
+
+IF COL_LENGTH('App.DataMigrations', 'UserName') IS NULL
+BEGIN
+	ALTER TABLE App.DataMigrations ADD UserName nvarchar(100);
+END
+
+IF COL_LENGTH('App.DataMigrations', 'DataMigrationTaskList') IS NULL
+BEGIN
+	ALTER TABLE App.DataMigrations ADD ReportList nvarchar(50);
+END
+
+IF COL_LENGTH('App.DataMigrationHistories', 'UserName') IS NULL
+BEGIN
+	ALTER TABLE App.DataMigrationHistories ADD UserName nvarchar(100);
+END
+
 DECLARE @AssessmentFactTypeId INT, @GenerateReportId INT
 SELECT @AssessmentFactTypeId = DimFactTypeId 
 FROM RDS.DimFactTypes 
