6784a3d6d66da1e80d5ececbe4761a27d101801d
johnnycunningham <jcunningham@aemcorp.com>
2024-08-21 11:59:13 -0400

    CNMI state name updates

--- generate.database/VersionUpdates/12.0_prerelease/ceds.TableChanges.sql
+++ generate.database/VersionUpdates/12.0_prerelease/ceds.TableChanges.sql
@@ -1,1 +1,5 @@
-update ceds.CedsOptionSetMapping set CedsOptionSetDescription = 'Northern Mariana Islands' where CedsOptionSetDescription = 'Northern Marianas'
+update ceds.CedsOptionSetMapping
+set CedsOptionSetDescription = 'Northern Mariana Islands',
+	CedsOptionSetDefinition =  'Northern Mariana Islands'
+where CedsElementTechnicalName in ('QualifyingMoveFromState', 'StateAbbreviation')
+and CedsOptionSetDefinition = 'Northern Marianas'
