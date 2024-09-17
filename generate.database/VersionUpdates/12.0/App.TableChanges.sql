490a711e43ff635b72744263622fe81d1813881c
johnnycunningham <jcunningham@aemcorp.com>
2024-09-17 10:10:18 -0400

    correct upgrade issue for v12

--- generate.database/VersionUpdates/12.0/App.TableChanges.sql
+++ generate.database/VersionUpdates/12.0/App.TableChanges.sql
@@ -40,6 +40,8 @@ END
 
 ALTER TABLE [App].[DataMigrationTasks] DROP CONSTRAINT [UX_DataMigrationTasks]
 
+SET ANSI_PADDING ON
+GO
 
 ALTER TABLE [App].[DataMigrationTasks] ADD  CONSTRAINT [UX_DataMigrationTasks] UNIQUE NONCLUSTERED 
 (
@@ -49,58 +51,7 @@ ALTER TABLE [App].[DataMigrationTasks] ADD  CONSTRAINT [UX_DataMigrationTasks] U
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 
 -- Remove Toggle Sections that have no questions
-delete from app.ToggleSections where SectionSequence in (202, 400, 401, 600, 601, 800, 801, 900, 901)
-
-
---------------------------------------------------------------------------
---Remove the duplicate question from the ToggleQuestions table (CIID-5657)
---------------------------------------------------------------------------
-
---first, set the new questions with the same response as the original question
-
-	declare @responseValue nvarchar(5)
-	set @responseValue = (select ResponseValue from App.ToggleResponses where ToggleQuestionId = 23)
-	if ISNULL(@responseValue, '') <> ''
-	begin
-
-		if (select count(*)
-			from app.ToggleResponses
-			where ToggleQuestionId = 61) = 1
-		begin
-			update App.ToggleResponses set ResponseValue = @responseValue where ToggleQuestionId = 61
-		end
-		else
-		begin
-			insert into App.ToggleResponses values (@responseValue, 61, NULL)
-		end
-
-		if (select count(*)
-			from app.ToggleResponses
-			where ToggleQuestionId = 62) = 1
-		begin
-			update App.ToggleResponses set ResponseValue = @responseValue where ToggleQuestionId = 62
-		end
-		else
-		begin
-			insert into App.ToggleResponses values (@responseValue, 62, NULL)
-		end
-
-	end
-
---second, remove the duplicate question
-
-	--ToggleResponses
-	delete from app.ToggleResponses
-	where ToggleQuestionId = 23
-
-	--ToggleQuestions
-	delete from app.ToggleQuestions
-	where ToggleQuestionId = 23
-
-	--ToggleSections
-	delete from app.ToggleSections
-	where ToggleSectionId = 29
-
+	delete from app.ToggleSections where SectionSequence in (202, 400, 401, 600, 601, 800, 801, 900, 901)
 
 --correct the report - fact type relationship for 150 and 151
 	delete 
@@ -110,12 +61,3 @@ delete from app.ToggleSections where SectionSequence in (202, 400, 401, 600, 601
 
 	insert into app.GenerateReport_FactType
 	values (75,18), (76,18)
-
-
-	
-
-
-
-
-
-
