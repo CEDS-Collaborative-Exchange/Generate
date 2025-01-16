0c77faef6d9ee3412a8cb126f9254f39c2018fa6
johnnycunningham <jcunningham@aemcorp.com>
2025-01-15 19:42:44 -0500

    Fix the report view for 221

--- generate.web/DatabaseScripts/Views/Create/RDS.vwNeglectedOrDelinquent_FactTable_221.View.sql
+++ generate.web/DatabaseScripts/Views/Create/RDS.vwNeglectedOrDelinquent_FactTable_221.View.sql
@@ -1,16 +1,22 @@
 CREATE VIEW [RDS].[vwNeglectedOrDelinquent_FactTable_221] 
 AS
-	SELECT
-        f.[SchoolYear]
-        , f.[K12StudentStudentIdentifierState]
-		, f.[NeglectedOrDelinquentStatusCode]
-		, f.[NeglectedOrDelinquentProgramEnrollmentSubpartCode]
-		, f.[EdFactsAcademicOrCareerAndTechnicalOutcomeTypeEdFactsCode]
-		, f.[StateANSICode]
-		, f.[StateAbbreviationCode]
-		, f.[StateAbbreviationDescription]
-		, f.[SeaOrganizationIdentifierSea]
-		, f.[SeaOrganizationName]
+	SELECT  f.[SchoolYear]
+			, f.[K12StudentStudentIdentifierState]
+			, f.[NeglectedOrDelinquentStatusCode]
+			, f.[NeglectedOrDelinquentProgramEnrollmentSubpartCode]
+			, f.[EdFactsAcademicOrCareerAndTechnicalOutcomeExitTypeEdFactsCode]
+			, f.[StateANSICode]
+			, f.[StateAbbreviationCode]
+			, f.[StateAbbreviationDescription]
+			, f.[SeaOrganizationIdentifierSea]
+			, f.[SeaOrganizationName]
+			, f.[LeaIdentifierSea]
+			, f.[LeaOrganizationName]
+			, f.[SchoolIdentifierSea]
+			, f.[DimK12SchoolId]
+			, f.[NameOfInstitution]
+			, f.[SchoolOperationalStatus]
+			, f.[SchoolTypeCode]
     FROM  [debug].[vwNeglectedOrDelinquent_FactTable] f
     WHERE f.NeglectedOrDelinquentStatusCode = 'Yes'
 	    AND f.NeglectedOrDelinquentProgramEnrollmentSubpartCode = '2'
