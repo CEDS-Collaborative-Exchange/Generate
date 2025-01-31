38a16b1841ba2ac2cce68906915746a39e3488d3
Sumanth Vuyuru <svuyuru@aemcorp.com>
2025-01-31 13:41:39 -0500

    release 12.2 updated changes.

--- generate.web/DatabaseScripts/VersionUpdates/12.2/App.Metadata.sql
+++ generate.web/DatabaseScripts/VersionUpdates/12.2/App.Metadata.sql
@@ -5,3 +5,15 @@ update App.FileColumns set DimensionId = @dimensionId where ColumnName = 'Certif
 
 SELECT @dimensionId = DimensionId FROM app.Dimensions WHERE DimensionFieldName = 'ParaprofessionalQualificationStatus'
 update App.FileColumns set DimensionId = @dimensionId where ColumnName = 'QualificationID'
+
+
+if not exists (select * from app.generatereport_organizationlevels where GenerateReportId = 136 and OrganizationLevelId = 1)
+begin 
+	insert into app.generatereport_organizationlevels values (136,1)
+end
+
+if not exists (select * from app.generatereport_organizationlevels where GenerateReportId = 136 and OrganizationLevelId = 2)
+begin 
+	Print 'here'
+	insert into app.generatereport_organizationlevels values (136,2)
+end
