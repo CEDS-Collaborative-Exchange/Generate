cea97c33deb3a4442b39c44df4684c26611eae5e
johnnycunningham <jcunningham@aemcorp.com>
2025-05-08 20:57:45 -0400

    Fix issues with DataMigrationType

--- generate.web/DatabaseScripts/Views/Create/Debug.vwDirectorySEA_FactTable.View.sql
+++ generate.web/DatabaseScripts/Views/Create/Debug.vwDirectorySEA_FactTable.View.sql
@@ -31,11 +31,10 @@ SELECT
 	--2 ways to select by SchoolYear, use 1 or the other, not both
 	--the next 2 conditions set the SchoolYear selected to the one from the most recent RDS migration
 		AND DMT.IsSelected = 1
-		AND DMT.DataMigrationTypeId = 2
+		AND DMT.DataMigrationTypeId = 3
 	--or comment out the lines above and just set the SchoolYear
 		--AND SchoolYears.SchoolYear = 2023
 
 	AND Fact.FactTypeId = 21
 	AND Fact.SeaId > -1
-	--AND SEAs.SeaOrganizationIdentifierSea = '789'
 
