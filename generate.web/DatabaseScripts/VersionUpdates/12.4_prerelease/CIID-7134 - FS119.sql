b55a6605b8dff022ff25e02be2ea2d9acf8000f9
sappikatla <siddarth.appikatla@aemcorp.com>
2025-05-11 23:40:04 -0400

    CIID-7134 File Updates
    CIID-7134 File Updates

---
+++ generate.web/DatabaseScripts/VersionUpdates/12.4_prerelease/CIID-7134 - FS119.sql
@@ -0,0 +1,8 @@
+﻿USE Generate
+Go
+
+ALTER TABLE [generate].[Staging].[ProgramParticipationTitleIII] ADD EnglishLearnersExitedStatus bit;
+ALTER TABLE [generate].[RDS].[DimTitleIIIStatuses] ADD EnglishLearnersExitedStatus bit;
+
+--UPDATE [generate].[Staging].[ProgramParticipationTitleIII] SET EnglishLearnersExitedStatus = 1;
+--UPDATE [generate].[RDS].[DimTitleIIIStatuses] SET EnglishLearnersExitedStatus = 1;
