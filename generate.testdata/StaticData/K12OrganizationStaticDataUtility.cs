31d8f0df4bee1a844346f093cb360478b0566c93
johnnycunningham <jcunningham@aemcorp.com>
2025-07-22 22:25:49 -0400

    Add LEA GFSA data

--- generate.testdata/StaticData/K12OrganizationStaticDataUtility.cs
+++ generate.testdata/StaticData/K12OrganizationStaticDataUtility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcf08c83c5bd6f6fd3eb21376e6740d507422128305631ccd080c0bd1443bcfb
-size 31478
+oid sha256:dd33c74c6d24f4b7bad63e1284744595e1f5caba929fdf0df9c82b9cc5322cb1
+size 33017
