d8a0f5ede25423041e29a5655bd7ff5c7c57ad0f
Sumanth Vuyuru <svuyuru@aemcorp.com>
2025-08-05 10:55:24 -0400

    CIID-8046 : Export and Metadata changes.

--- generate.web/DatabaseScripts/VersionUpdates/13.0_prerelease/App.Metadata.sql
+++ generate.web/DatabaseScripts/VersionUpdates/13.0_prerelease/App.Metadata.sql
@@ -1,2 +1,17 @@
 
+declare @categoryId as INT, @dimensionId as INT
+
 Update app.GenerateConfigurations set GenerateConfigurationValue = '2025' where GenerateConfigurationKey = 'SchoolYear'
+
+select @dimensionId = DimensionId from app.Dimensions where DimensionFieldName = 'ISO6392LanguageCode'
+select @categoryId = CategoryId from app.Categories where CategoryCode = 'LANGHOME'
+
+IF NOT EXISTS(SELECT 1 FROM app.Category_Dimensions where CategoryId = @categoryId and DimensionId = @dimensionId)
+BEGIN
+    INSERT INTO [App].[Category_Dimensions]
+               ([CategoryId]
+               ,[DimensionId])
+         VALUES (@categoryId, @dimensionId)
+END
+
+exec App.UpdateViewDefinitions
