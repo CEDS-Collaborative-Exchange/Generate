d50db0d0f1f8d83747116c7edf541f0d11fb05eb
johnnycunningham <jcunningham@aemcorp.com>
2025-10-07 23:17:21 -0400

    School Performance Indicators structure

--- generate.web/DatabaseScripts/StoredProcedures/Create/Staging.Staging-to-DimSchoolPerformanceIndicatorStateDefinedStatuses.StoredProcedure.sql
+++ generate.web/DatabaseScripts/StoredProcedures/Create/Staging.Staging-to-DimSchoolPerformanceIndicatorStateDefinedStatuses.StoredProcedure.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [Staging].[Staging-to-DimSchoolPerformanceIndicatorStateDefinedStatusestateDefinedStatuses]
+﻿CREATE PROCEDURE [Staging].[Staging-to-DimSchoolPerformanceIndicatorStateDefinedStatuses]
 AS
 BEGIN
 
