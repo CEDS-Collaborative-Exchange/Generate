b943951c0ff34878bbbee8b97a3a3f7a0dc615c5
svuyuru <sumanth.vuyuru@aemcorp.com>
2025-12-02 15:23:23 -0500

    CIID-8162 : Fix the 029 Columns issue.

---
+++ generate.web/DatabaseScripts/VersionUpdates/13.2_prerelease/App.Metadata.sql
@@ -0,0 +1,3 @@
+ Update app.FileColumns set ReportColumn = 'MailingAddressCity' where ColumnName = 'MailingCity'
+ Update app.FileColumns set ReportColumn = 'MailingAddressPostalCode' where ColumnName = 'MailingZipcode'
+ Update app.FileColumns set ReportColumn = 'MailingAddressState' where ColumnName = 'MailingPostalStateCode'
