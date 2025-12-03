e24378454a52da9925294bd5f1c5e5ba323753d2
svuyuru <sumanth.vuyuru@aemcorp.com>
2025-12-03 12:14:27 -0500

    CIID-8083 : Metadata backup process changes.

--- generate.infrastructure/Repositories/App/AppRepository.cs
+++ generate.infrastructure/Repositories/App/AppRepository.cs
@@ -479,5 +479,13 @@ namespace generate.infrastructure.Repositories.App
             _context.Database.ExecuteSqlRaw("app.Enable_Disable_Tests @testScope = {0}, @isActive = {1}", fileSpecNumbers, enable);
             _context.Database.SetCommandTimeout(oldTimeOut);
         }
+
+        public void MigrateMetadata(int submissionYear, bool isTransferAppToMetadata = true)
+        {
+            int? oldTimeOut = _context.Database.GetCommandTimeout();
+            _context.Database.SetCommandTimeout(11000);
+            _context.Database.ExecuteSqlRaw("app.Migrate_Metadata @submissionYear = {0}, @isTransferAppToMetadata = {1}", submissionYear, isTransferAppToMetadata);
+            _context.Database.SetCommandTimeout(oldTimeOut);
+        }
     }
 }
---
+++ generate.web/DatabaseScripts/StoredProcedures/Create/App.Migrate_Metadata.StoredProcedure.sql
@@ -0,0 +1,72 @@
+﻿CREATE PROCEDURE [App].[Migrate_Metadata]
+	@isTransferAppToMetadata bit = 1,
+	@submissionYear int
+AS
+BEGIN
+
+
+	IF @isTransferAppToMetadata = 1
+	BEGIN
+
+		insert into Metadata.CategorySets
+		select * from app.CategorySets where SubmissionYear = @submissionYear
+
+		insert into Metadata.CategorySet_Categories
+		select * from app.CategorySet_Categories
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		insert into Metadata.CategoryOptions
+		select * from app.CategoryOptions
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		insert into Metadata.FileSubmissions
+		select * from app.FileSubmissions where SubmissionYear = @submissionYear
+
+		insert into Metadata.FileSubmission_FileColumns
+		select * from app.FileSubmission_FileColumns 
+		where FileSubmissionId in (select distinct FileSubmissionId from app.FileSubmissions where SubmissionYear = @submissionYear)
+
+	END
+	ELSE
+	BEGIN
+		
+		delete from app.CategoryOptions
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		delete from app.CategorySet_Categories
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		delete from app.CategorySets where SubmissionYear = @submissionYear
+
+		delete from app.FileSubmission_FileColumns 
+		where FileSubmissionId in (select distinct FileSubmissionId from app.FileSubmissions where SubmissionYear = @submissionYear)
+
+		delete from app.FileSubmissions where SubmissionYear = @submissionYear
+
+		insert into app.CategorySets
+		select * from Metadata.CategorySets where SubmissionYear = @submissionYear
+
+		insert into app.CategorySet_Categories
+		select * from Metadata.CategorySet_Categories
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		insert into app.CategoryOptions
+		select * from Metadata.CategoryOptions
+		where CategorySetId in (select distinct CategorySetId from app.CategorySets where SubmissionYear = @submissionYear)
+
+		insert into app.FileSubmissions
+		select * from Metadata.FileSubmissions where SubmissionYear = @submissionYear
+
+		insert into app.FileSubmission_FileColumns
+		select * from Metadata.FileSubmission_FileColumns 
+		where FileSubmissionId in (select distinct FileSubmissionId from app.FileSubmissions where SubmissionYear = @submissionYear)
+
+		drop table Metadata.CategoryOptions
+		drop table Metadata.CategorySet_Categories
+		drop table Metadata.CategorySets
+		drop table Metadata.FileSubmission_FileColumns
+		drop table Metadata.FileSubmissions
+
+	END
+	
+END
