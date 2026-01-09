5ea2c5fceeab599f4cfe96a2b3fa2ac74eddd38c
johnnycunningham <jcunningham@aemcorp.com>
2026-01-09 09:51:26 -0500

    changes for 138

--- generate.web/DatabaseScripts/VersionUpdates/13.2_prerelease/JunkTablePopulation.sql
+++ generate.web/DatabaseScripts/VersionUpdates/13.2_prerelease/JunkTablePopulation.sql
@@ -1,3 +1,6 @@
+-----------------------------------------------
+--Populate DimPsEnrollmentStatuses
+-----------------------------------------------
 	--Remove the existing dimension values
 	DELETE FROM	RDS.DimPsEnrollmentStatuses
 
@@ -95,4 +98,72 @@
 	DROP TABLE #PostsecondaryExitOrWithdrawalType
 	DROP TABLE #PostsecondaryEnrollmentStatus
 	DROP TABLE #PostsecondaryEnrollmentAction
+
+-----------------------------------------------
+--Populate DimAssessmentStatuses
+-----------------------------------------------
+
+	--Remove the existing dimension values
+	DELETE FROM	RDS.DimAssessmentStatuses
+
+	--Repopulate the dimension with the new table values added
+	IF NOT EXISTS (SELECT 1 FROM RDS.DimAssessmentStatuses WHERE DimAssessmentStatusId = -1) BEGIN
+		SET IDENTITY_INSERT RDS.DimAssessmentStatuses ON
+
+		INSERT INTO [RDS].DimAssessmentStatuses (
+			[DimAssessmentStatusId]
+			, [ProgressLevelCode]
+			, [ProgressLevelDescription]
+			, [ProgressLevelEdFactsCode]
+			, [AssessedFirstTimeCode]
+			, [AssessedFirstTimeDescription]
+			, [AssessedFirstTimeEdFactsCode]
+		)
+		VALUES (
+			-1
+			, 'MISSING'
+			, 'MISSING'
+			, 'MISSING'
+			, 'MISSING'
+			, 'MISSING'
+			, 'MISSING'
+		)
+
+		SET IDENTITY_INSERT RDS.DimAssessmentStatuses OFF
+	END
+
+	--ProgressLevel
+	CREATE TABLE #ProgressLevel (ProgressLevelCode VARCHAR(50), ProgressLevelDescription VARCHAR(200), ProgressLevelEdFactsCode VARCHAR(50))
+	INSERT INTO #ProgressLevel VALUES ('MISSING', 'MISSING', 'MISSING')
+	INSERT INTO #ProgressLevel
+	SELECT 
+		  CedsOptionSetCode
+		, CedsOptionSetDescription
+		, CedsOptionSetCode
+	FROM [CEDS].CedsOptionSetMapping
+	WHERE CedsElementTechnicalName = 'ProgressLevel'
+
+	INSERT INTO RDS.DimAssessmentStatuses (
+		[ProgressLevelCode]
+		, [ProgressLevelDescription]
+		, [ProgressLevelEdFactsCode]
+		, [AssessedFirstTimeCode]
+		, [AssessedFirstTimeDescription]
+		, [AssessedFirstTimeEdFactsCode]
+	)
+	SELECT DISTINCT
+		  a.ProgressLevelCode
+		, a.ProgressLevelDescription
+		, a.ProgressLevelEdFactsCode
+		, b.CedsOptionSetCode
+		, b.CedsOptionSetDescription
+		, b.EdFactsCode
+	FROM (VALUES('Yes', 'Yes', 'FIRSTASSESS'),('No', 'No', 'MISSING'),('MISSING', 'MISSING', 'MISSING')) b(CedsOptionSetCode, CedsOptionSetDescription, EdFactsCode)
+	CROSS JOIN #ProgressLevel a
+	LEFT JOIN RDS.DimAssessmentStatuses main
+		ON a.ProgressLevelCode = main.ProgressLevelCode
+		AND b.CedsOptionSetCode = main.AssessedFirstTimeCode
+	WHERE main.DimAssessmentStatusId IS NULL
+
+	DROP TABLE #ProgressLevel
 
