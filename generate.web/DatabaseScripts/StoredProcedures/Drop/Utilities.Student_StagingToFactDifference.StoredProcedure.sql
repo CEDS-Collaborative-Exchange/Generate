197665196e94ebd1f92c5aab5e95cbb54da9fd1b
johnnycunningham <jcunningham@aemcorp.com>
2026-03-09 12:01:53 -0400

    New utility

---
+++ generate.web/DatabaseScripts/StoredProcedures/Drop/Utilities.Student_StagingToFactDifference.StoredProcedure.sql
@@ -0,0 +1,13 @@
+﻿
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_SCHEMA = 'Utilities' AND ROUTINE_NAME = 'Student_StagingToFactDifference') 
+BEGIN
+	DROP PROCEDURE [Utilities].[Student_StagingToFactDifference]
+END
+
+
+
+
+
+
+
+
