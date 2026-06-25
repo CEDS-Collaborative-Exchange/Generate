ccbe11b4ca771d6c6557ed2cb6cce9f3c764dcb0
johnnycunningham <jcunningham@aemcorp.com>
2026-06-25 09:23:58 -0400

    Clean up data lengths

--- generate.web/DatabaseScripts/VersionUpdates/14.0_prerelease/Staging.TableChanges.sql
+++ generate.web/DatabaseScripts/VersionUpdates/14.0_prerelease/Staging.TableChanges.sql
@@ -1,4 +1,19 @@
---Add changes
+--Align the column lengths between staging and the RDS
+IF COL_LENGTH('Staging.K12Enrollment', 'FirstName') IS NOT NULL
+BEGIN
+    ALTER TABLE Staging.K12Enrollment ALTER COLUMN FirstName NVARCHAR(75) NULL;
+END
+
+IF COL_LENGTH('Staging.K12Enrollment', 'MiddleName') IS NOT NULL
+BEGIN
+    ALTER TABLE Staging.K12Enrollment ALTER COLUMN MiddleName NVARCHAR(75) NULL;
+END
+
+IF COL_LENGTH('Staging.K12Enrollment', 'LastOrSurname') IS NOT NULL
+BEGIN
+    ALTER TABLE Staging.K12Enrollment ALTER COLUMN LastOrSurname NVARCHAR(75) NULL;
+END
+
 
 
 IF COL_LENGTH('Staging.K12Enrollment', 'NumberOfSchoolDays') IS NOT NULL
