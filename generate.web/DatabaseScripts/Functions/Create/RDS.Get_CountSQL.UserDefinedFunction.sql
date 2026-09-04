378114304689b79705d39a4687055a4e0204aca1
johnnycunningham <jcunningham@aemcorp.com>
2026-09-04 18:45:59 -0400

    update get_countSQL for assessments

--- generate.web/DatabaseScripts/Functions/Create/RDS.Get_CountSQL.UserDefinedFunction.sql
+++ generate.web/DatabaseScripts/Functions/Create/RDS.Get_CountSQL.UserDefinedFunction.sql
@@ -8277,7 +8277,7 @@ BEGIN
 				delete a from @reportData a
 				where a.' + @factField + ' = 0
 				AND RIGHT(a.TableTypeAbbrv, 2) = ''LG''
-				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR NOT IN (''REGPARTWOACC'', ''REGPARTWACC'', ''ALTPARTALTACH'', ''MEDEXEMPT'', ''NPART'')
+				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR NOT IN (''REGPARTWOACC'', ''REGPARTWACC'', ''ALTPARTALTACH'', ''MEDEXEMPT'', ''NPART'',''PARTELP'' )
 				AND NOT EXISTS (Select 1 from app.ToggleAssessments b
 									where a.GradeLevel = b.Grade
 									and a.AssessmentAcademicSubject = b.Subject
@@ -8298,7 +8298,7 @@ BEGIN
 				delete a from @reportData a
 				where a.' + @factField + ' = 0
 				AND RIGHT(a.TableTypeAbbrv, 2) = ''HS''
-				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR NOT IN (''ALTPARTALTACH'', ''MEDEXEMPT'', ''NPART'')
+				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR NOT IN (''ALTPARTALTACH'', ''MEDEXEMPT'', ''NPART'',''PARTELP'')
 				AND NOT EXISTS (Select 1 from app.ToggleAssessments b
 									where a.GradeLevel = b.Grade
 									and a.AssessmentAcademicSubject = b.Subject
@@ -8307,7 +8307,7 @@ BEGIN
 			set @sql = @sql + ' 
 				delete a from @reportData a
 				where a.' + @factField + ' = 0
-				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR IN (''MEDEXEMPT'', ''NPART'')
+				AND a.ASSESSMENTREGISTRATIONPARTICIPATIONINDICATOR IN (''MEDEXEMPT'', ''NPART'',''PARTELP'')
 				AND NOT EXISTS (Select distinct b.Grade 
 									from app.ToggleAssessments b
 									where a.GradeLevel = b.Grade
